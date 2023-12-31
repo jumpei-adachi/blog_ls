3f6c24b33645d59f566d6fbb7a2592a4e6f3b1bf
Jumpei Adachi <jumpei.adachi@icloud.com>
2023-12-31 12:03:58 +0900

    作成日時(birthtime)ではなく最終状態変更日時(ctime)によってソートされていたので修正

--- bls.rb
+++ bls.rb
@@ -3,13 +3,13 @@
 # 作成日時の降順でhtmlファイルのリストを作成
 paths = Dir.glob('*.html')
   .select {|path| path != 'blog-index.html' }
-  .sort_by {|path| File::Stat.new(path).ctime }
+  .sort_by {|path| File::Stat.new(path).birthtime }
   .reverse
 
 paths.each do |path|
   short_file_name = path[0, 6]
 
-  creation_time = File::Stat.new(path).ctime
+  creation_time = File::Stat.new(path).birthtime
 
   # title要素のテキスト内容を正規表現を用いて取得
   title = /<title>(.*)<\/title>/
